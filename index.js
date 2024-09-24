f6bbd3f49bed716c70c50355f60628aa7157f375
geliaswilson <geliaswilson@gmail.com>
2024-09-24 05:36:46 -0400

    stories.json information now successfully being shown by get requests, added images to the stories

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 import express from 'express';
 import cors from 'cors';
 import "dotenv/config";
-app = express();
+import fs from "fs";
+const app = express();
 let {PORT, CROSS_ORIGIN} = process.env;
 PORT = PORT || 8080;
 app.use(cors({origin:CROSS_ORIGIN}));
+app.use(express.json());
+const readStories = () => {
+    const storiesFile = fs.readFileSync("./data/stories.json")
+    const storiesData = JSON.parse(storiesFile);
+    return storiesData
+}
+app.get("/", (req, res) => {
+    res.json(readStories());
+})
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+})
