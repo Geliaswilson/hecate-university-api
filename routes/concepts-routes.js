e4f957e95d7cf7a805626369c53584b5f289696e
geliaswilson <geliaswilson@gmail.com>
2024-09-29 15:00:12 -0400

    edit .route in concepts-routes to be "/"

--- routes/concepts-routes.js
+++ routes/concepts-routes.js
@@ -1,5 +1,5 @@
 import express from "express";
 import * as conceptsController from "../controllers/concepts-controller.js";
 const conceptsRouter = express.Router();
-conceptsRouter.route("/concepts").get(conceptsController.readConcepts);
+conceptsRouter.route("/").get(conceptsController.readConcepts);
 export default conceptsRouter;
